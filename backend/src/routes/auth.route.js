d24957e6356840cfa529e5ab8c922f0564aa8e15
Ali <hoccalo@gmail.com>
2025-09-12 03:57:46 +0300

    update profile

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { login, logout, signup } from "../controllers/auth.controller.js";
+import { login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 
 router.post("/signup", signup);
@@ -7,4 +9,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
 export default router;
