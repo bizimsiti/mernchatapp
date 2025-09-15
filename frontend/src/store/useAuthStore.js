fdf46263c876b5a6ce072c7ed05226f07b4baed9
Ali <hoccalo@gmail.com>
2025-09-15 23:41:15 +0300

    logout

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -31,5 +31,14 @@ export const useAuthStore = create((set) => ({
     } finally {
       set({ isSigninUp: false });
     }
+  },
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
   }
 }));
