304c44981ed68ea6ef59140707b2bab7674576e3
Ali <hoccalo@gmail.com>
2025-09-16 17:03:11 +0300

    sidebar components

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -29,5 +29,6 @@ export const useChatStore = create((set) => ({
     } finally {
       set({ isMessagesLoading: false });
     }
-  }
+  },
+  setSelectedUser: (selectedUser) => set({ selectedUser })
 }));
