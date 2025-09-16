19c2c14083d8002788be61fcef2b5f017e39a004
Ali <hoccalo@gmail.com>
2025-09-16 23:02:30 +0300

    chat component formattime

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
+import { formatMessageTime } from "../lib/utils.js";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
 import { useChatStore } from "../store/useChatStore";
 import { useAuthStore } from "../store/useAuthStore";
@@ -34,7 +35,9 @@ const ChatContainer = () => {
                 />
               </div>
             </div>
-            <div className="chat-header mb-1"></div>
+            <div className="chat-header mb-1">
+              <time className="text-xs opacity-50 ml-1">{formatMessageTime(message.createdAt)}</time>
+            </div>
             <div className="chat-bubble flex flex-col">
               {message.image && (
                 <img src={message.image} alt="Attachment" className="sm:max-w-[200px] rounded-md mb-2" />
